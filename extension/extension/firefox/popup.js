7181c6da0976a832d9af3ecbf31df3a32e60ccb4
Renan Kalu <88622721+AxoloteDev@users.noreply.github.com>
2023-07-01 23:31:27 -0300

    Add files via upload

---
+++ extension/extension/firefox/popup.js
@@ -0,0 +1,9 @@
+document.querySelector("button").addEventListener("click", () => {
+	browser.windows.create({
+		url: "https://simple-pomodoro-delta.vercel.app/",
+		left: Math.round((screen.width - 800) / 2), // Largura da janela: 800px
+		top: Math.round((screen.height - 600) / 2), // Altura da janela: 600px
+		width: 800,
+		height: 800,
+	});
+});
