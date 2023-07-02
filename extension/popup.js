e73bae6dd07c396153f193a998c94ffd354495f3
Renan Kalu <88622721+AxoloteDev@users.noreply.github.com>
2023-07-01 23:33:19 -0300

    Delete popup.js

--- extension/popup.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("button").addEventListener("click", () => {
-	chrome.windows.create({
-		url: "https://simple-pomodoro-delta.vercel.app/",
-		left: Math.round((screen.width - 800) / 2), // Largura da janela: 800px
-		top: Math.round((screen.height - 600) / 2), // Altura da janela: 600px
-		width: 800,
-		height: 800,
-	});
-});
